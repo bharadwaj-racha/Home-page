c90ee6ad9b3c78fbb274379d1891e3507099d046
Racha Bharadwaj <rachabharadwajgupta@gmail.com>
2024-07-20 18:17:06 +0530

    Your commit message

---
+++ src/App.jsx
@@ -0,0 +1,143 @@
+import secondimg from './assets/sf.png';
+import firstimg from './assets/ff.png';
+
+function App() {
+  return (
+    <>
+    <section class='center section1 py-4 px-5'>
+    <h2 class='main-head mt-5'>EMPOWERING THE DIGITAL ERA:<br></br>DECENTRALIZING ENTERTAINMENT</h2>
+    <div class='sub-text mt-5 fw-medium'>Join the world's first DePIN:Revolutionizing Content Streaming with faster,safer <br></br>and Cheaper Access</div>
+    <div class='d-flex justify-content-center'>
+      <div class='button mt-5 fs-6 fw-semibold p-2 px-3 rounded-5'>
+        Coming Soon
+        <span class='ms-1'>
+          <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M12 2.04492C6.48 2.04492 2 6.52492 2 12.0449C2 17.5649 6.48 22.0449 12 22.0449C17.52 22.0449 22 17.5649 22 12.0449C22 6.52492 17.52 2.04492 12 2.04492ZM16.03 12.5749L13.03 15.5749C12.88 15.7249 12.69 15.7949 12.5 15.7949C12.31 15.7949 12.12 15.7249 11.97 15.5749C11.68 15.2849 11.68 14.8049 11.97 14.5149L13.69 12.7949H8.5C8.09 12.7949 7.75 12.4549 7.75 12.0449C7.75 11.6349 8.09 11.2949 8.5 11.2949H13.69L11.97 9.57492C11.68 9.28492 11.68 8.80492 11.97 8.51492C12.26 8.22492 12.74 8.22492 13.03 8.51492L16.03 11.5149C16.32 11.8049 16.32 12.2849 16.03 12.5749Z" fill="#EDF0FF"/>
+          </svg>
+        </span>
+      </div>
+    </div>
+    <img class='my-3' src={firstimg} alt="second fold image" />
+    <div class='row mt-5'>
+      <div class='col-md-6 col-12'>
+        <div class='m-2 rounded-3 box p-2 px-4'>
+          <div class='d-flex justify-content-between'>
+          <div class='box-text'>
+            No.of Node Operators
+          </div>
+          <div>
+              <svg width="47" height="48" viewBox="0 0 47 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M23.5 1.5V46.5" stroke="url(#paint0_linear_4_5410)" stroke-width="2" stroke-linecap="round"/>
+              <path d="M1 24L46 24" stroke="url(#paint1_linear_4_5410)" stroke-width="2" stroke-linecap="round"/>
+              <defs>
+              <linearGradient id="paint0_linear_4_5410" x1="24" y1="1.5" x2="24" y2="46.5" gradientUnits="userSpaceOnUse">
+              <stop stop-color="white" stop-opacity="0"/>
+              <stop offset="0.5" stop-color="#1E1E1E"/>
+              <stop offset="1" stop-color="white" stop-opacity="0"/>
+              </linearGradient>
+              <linearGradient id="paint1_linear_4_5410" x1="1" y1="24" x2="46" y2="24" gradientUnits="userSpaceOnUse">
+              <stop stop-color="white" stop-opacity="0"/>
+              <stop offset="0.505" stop-color="#1E1E1E"/>
+              <stop offset="1" stop-color="white"/>
+              </linearGradient>
+              </defs>
+              </svg>
+          </div>
+          </div>
+          <div class='box-text2 fw-semibold'>2000</div>
+        </div>
+      </div>
+      <div class='col-md-6 col-12'>
+        <div class='m-2 rounded-3 box p-2 px-4'>
+          <div class='d-flex justify-content-between'>
+          <div class='box-text'>
+            No.of Node Operators
+          </div>
+          <div>
+              <svg width="47" height="48" viewBox="0 0 47 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M23.5 1.5V46.5" stroke="url(#paint0_linear_4_5410)" stroke-width="2" stroke-linecap="round"/>
+              <path d="M1 24L46 24" stroke="url(#paint1_linear_4_5410)" stroke-width="2" stroke-linecap="round"/>
+              <defs>
+              <linearGradient id="paint0_linear_4_5410" x1="24" y1="1.5" x2="24" y2="46.5" gradientUnits="userSpaceOnUse">
+              <stop stop-color="white" stop-opacity="0"/>
+              <stop offset="0.5" stop-color="#1E1E1E"/>
+              <stop offset="1" stop-color="white" stop-opacity="0"/>
+              </linearGradient>
+              <linearGradient id="paint1_linear_4_5410" x1="1" y1="24" x2="46" y2="24" gradientUnits="userSpaceOnUse">
+              <stop stop-color="white" stop-opacity="0"/>
+              <stop offset="0.505" stop-color="#1E1E1E"/>
+              <stop offset="1" stop-color="white"/>
+              </linearGradient>
+              </defs>
+              </svg>
+          </div>
+          </div>
+          <div class='box-text2 fw-semibold'>2000</div>
+        </div>
+      </div>
+    </div> 
+    </section>
+    <section class='section2 px-5 py-4 center'>
+      <div class='sf-text fw-medium mt-5'>FanCloud CDN network revolutionizes video infrastructure<br></br> with community-driven Innovation</div>
+      <div class='sf-sub-text mt-4 fw-regular'>FanCloud pioneers a new era of connectivity with its decentralised content delivery network (dCDN), leveraging a global network of community-powered nodes. By harnessing a decentralised architecture, dCDN drastically reduces data transfer and compute costs while enhancing load time. Join FanCloud’s dCDN network and experience the future of seamless, secure and community- driven consumption</div>
+      <img class='my-5' src={secondimg} alt="second fold image" />
+      <div class='row mt-5'>
+        <div class='col-md-4 col-12'>
+          <div class='m-3 sf-box1 rounded-4 py-5 p-1 px-3'>
+            <div class='sf-text'>
+              Content Nodes
+            </div>
+            <div class='mt-3 mb-5'>
+              Content Nodes, Earn rewards for supplying their GPU, Storage and bandwidth for dCDN
+            </div>
+            <div class='d-flex justify-content-end'>
+            <div class='button2 mt-4 fs-6 fw-semibold p-2 px-3 rounded-5'>
+              Coming Soon
+              <span class='ms-1'>
+                <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M12 2.04492C6.48 2.04492 2 6.52492 2 12.0449C2 17.5649 6.48 22.0449 12 22.0449C17.52 22.0449 22 17.5649 22 12.0449C22 6.52492 17.52 2.04492 12 2.04492ZM16.03 12.5749L13.03 15.5749C12.88 15.7249 12.69 15.7949 12.5 15.7949C12.31 15.7949 12.12 15.7249 11.97 15.5749C11.68 15.2849 11.68 14.8049 11.97 14.5149L13.69 12.7949H8.5C8.09 12.7949 7.75 12.4549 7.75 12.0449C7.75 11.6349 8.09 11.2949 8.5 11.2949H13.69L11.97 9.57492C11.68 9.28492 11.68 8.80492 11.97 8.51492C12.26 8.22492 12.74 8.22492 13.03 8.51492L16.03 11.5149C16.32 11.8049 16.32 12.2849 16.03 12.5749Z" fill="#0C091B"/>
+                </svg>
+              </span>
+            </div>
+          </div>
+          </div>
+        </div>
+        <div class='col-md-4 col-12'>
+          <div class='m-3 sf-box2 rounded-4 py-5 p-1 px-3'>
+            <div class='sf-text'>
+            Orchestrators
+            </div>
+            <div class='mt-3'>
+            Orchestrators will be responsible for connecting the client with the content node for seamless streaming. Traffic distribution will be based on multiple factors like job type, performance            </div>
+            <div class='d-flex justify-content-end'>
+            <div class='button3 mt-4 fs-6 fw-semibold p-2 px-3 rounded-5'>
+              Coming Soon
+            </div>
+          </div>
+          </div>
+        </div>
+        <div class='col-md-4 col-12'>
+          <div class='m-3 sf-box3 rounded-4 py-5 p-1 px-3'>
+            <div class='sf-text'>
+            Validators
+            </div>
+            <div class='mt-3'>
+            Validators will be responsible for validating the execution of dCDN Network. Validators will earn rewards from Staking and earn a cut on the executions validated.            </div>
+            <div class='d-flex justify-content-end'>
+            <div class='button4 mt-4 fs-6 fw-semibold p-2 px-3 rounded-5'>
+              Coming Soon
+            </div>
+          </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class='center section3 px-5 py-4'>
+        <div class='sf-text py-5'>The transition CDN-dCDN</div>
+        <div class='sf-sub-text pb-5'>FanCloud pioneers a new era of connectivity with its decentralised content delivery network (dCDN), leveraging a global network of community-powered nodes. By harnessing a decentralised architecture, dCDN drastically reduces data transfer and compute costs while enhancing load time. Join FanCloud’s dCDN network and experience the future of seamless, secure and community- driven consumption</div>
+    </section>
+    </>
+  )
+}
+
+export default App
---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
+
+import App from './App.jsx';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
